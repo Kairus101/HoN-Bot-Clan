321b82f163624d272ba94c57194854840ffd42c0
Kairus <kerey14@hotmail.com>
2013-04-24 23:08:35 +1200

    added descriptions

--- jungleLib.lua
+++ jungleLib.lua
@@ -11,22 +11,26 @@ local ceil, floor, pi, tan, atan, atan2, abs, cos, sin, acos, max, random
 
 local BotEcho, VerboseLog, BotLog = core.BotEcho, core.VerboseLog, core.BotLog
 
+leigonSpots={
+{pos={7200,3600},	description="L closest to well"			},
+{pos={7800,4500},	description="L easy camp"				},
+{pos={9800,4200},	description="L mid-jungle hard camp"	},
+{pos={11100,3100},	description="L pullable camp"			},
+{pos={11300,4400},	description="L camp above pull camp"	},
+{pos={5100,8000},	description="L ancients"				}
+}
+
+hellbourneSpots={
+{pos={9400,11200},	description="H closest to well"			}	,
+{pos={7700,11600},	description="H easy camp"				}	,
+{pos={6600,10500},	description="H below easy camp"			}	,
+{pos={5100,12500},	description="H pullable camp"			}	,
+{pos={4000,11500},	description="H far hard camp"			}	,--far hard
+{pos={12300,5600},	description="H ancients"				}	--ancients
+}
+
 function object.jungle.assess(botBrain)
-	leigonSpots={
-	{7200,3600},--closest
-	{7800,4500},--easy
-	{9800,4200},--hard
-	{11100,3100},--pull
-	{11300,4400}--above pull
-	{5100,8000}--ancients
-	}
 	
-	hellbourneSpots={
-	{9400,11200},--closest
-	{7700,11600},--easy
-	{6600,10500}--below easy
-	{5100,12500},--pull
-	{4000,11500}--far hard
-	{12300,5600}--ancients
-	}
+	
+	
 end
