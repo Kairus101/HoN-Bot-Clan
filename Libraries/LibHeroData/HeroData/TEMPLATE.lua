1e0617fe411d36300cf11448073d14d2457384ac
Zerotorescue <zero@isimba.org>
2013-06-27 01:06:55 +0200

    Update Template file
    Added the CastEffectType property to the property list. Also improved the general structure a fair bit and entered some example values in the TargetType properties. This should make creating a new hero data file easier.

--- Libraries/LibHeroData/HeroData/TEMPLATE.lua
+++ Libraries/LibHeroData/HeroData/TEMPLATE.lua
@@ -6,25 +6,56 @@ require('/bots/Libraries/LibHeroData/Classes/AbilityInfo.class.lua');
 local classes = _G.HoNBots.Classes;
 local HeroInfo, AbilityInfo = classes.HeroInfo, classes.AbilityInfo;
 
--- HERONAME
+-- HeroNameInGame
 local hero = HeroInfo.Create('Hero_TYPENAME');
 hero.Threat = 2;
 
-do -- FirstAbilityName
-	local abil = AbilityInfo.Create(0, 'Ability_TYPENAME1');
-	abil.Threat = 2;
-	abil.TargetType = 'UNKNOWN'; -- Passive, Self, AutoCast, TargetUnit, TargetPosition, TargetVector, VectorEntity
+do -- FirstAbilityNameInGame
+	local abil = AbilityInfo.Create(3, 'Ability_TYPENAME4');
+	abil.Threat = 0;
+	abil.TargetType = 'Passive,Self,AutoCast,TargetUnit,TargetPosition,TargetVector,VectorEntity';
+	abil.CastEffectType = 'Magic,Physical,SuperiorMagic,SuperiorPhysical';
+	-- Below are all the possible properties, scroll further down for comments explaining all of them.
+	-- Remove any properties that should stay default. Below values are NOT the default values, they're likely values to make copy pasting easier.
+	-- (remove this comment block when copy-pasting)
+	abil.VectorEntityTarget = 'Hero';
+	abil.CanCastOnSelf = true;
+	abil.CanCastOnFriendlies = true;
 	abil.CanCastOnHostiles = true;
+	abil.ChannelingState = 'State_Hero_Ability_SelfCast';
+	abil.CanStun = true;
+	abil.CanInterrupt = true;
+	abil.CanInterruptMagicImmune = true;
 	abil.CanSlow = true;
-	abil.MagicDPS = { 4, 8, 12, 16 };
-	abil.Debuff = 'State_Arachna_Ability1';
+	abil.CanRoot = true;
+	abil.CanDisarm = true;
+	abil.CanTurnInvisible = true;
+	abil.CanReveal = true;
+	abil.CanDispositionSelf = true;
+	abil.CanDispositionFriendlies = true;
+	abil.CanDispositionHostiles = true;
+	abil.StunDuration = 1000;
+	abil.ShouldSpread = true;
+	abil.ShouldInterrupt = true;
+	abil.ShouldBreakFree = true;
+	abil.ShouldPort = true;
+	abil.ShouldAvoidDamage = true;
+	abil.ShouldRemoveNullStone = true;
+	abil.MagicDamage = { 120, 140, 160, 180 };
+	abil.MagicDPS = { 4, 8, 16, 24 };
+	abil.PhysicalDamage = { 210, 250, 300, 350 };
+	abil.PhysicalDPS = { 5, 10, 15, 20 };
+	abil.Buff = 'State_Name_Here_Buff';
+	abil.BuffDuration = 2000;
+	abil.Debuff = 'State_Name_Here_Debuff';
+	abil.DebuffDuration = 2000;
 	hero:AddAbility(abil);
 end
 
 do -- SecondAbilityName
 	local abil = AbilityInfo.Create(1, 'Ability_TYPENAME2');
 	abil.Threat = 0;
-	abil.TargetType = 'UNKNOWN'; -- Passive, Self, AutoCast, TargetUnit, TargetPosition, TargetVector, VectorEntity
+	abil.TargetType = 'Self';
 	abil.CanCastOnSelf = true;
 	abil.CanCastOnFriendlies = true;
 	hero:AddAbility(abil);
@@ -33,7 +64,7 @@ end
 do -- ThirdAbilityName
 	local abil = AbilityInfo.Create(2, 'Ability_TYPENAME3');
 	abil.Threat = 0; -- The threat for this ability is automatically calculated by the DPS threat
-	abil.TargetType = 'UNKNOWN'; -- Passive, Self, AutoCast, TargetUnit, TargetPosition, TargetVector, VectorEntity
+	abil.TargetType = 'Passive';
 	abil.Buff = 'State_Arachna_Ability3';
 	hero:AddAbility(abil);
 end
@@ -41,7 +72,8 @@ end
 do -- UltimateName
 	local abil = AbilityInfo.Create(3, 'Ability_TYPENAME4');
 	abil.Threat = 2;
-	abil.TargetType = 'UNKNOWN'; -- Passive, Self, AutoCast, TargetUnit, TargetPosition, TargetVector, VectorEntity
+	abil.TargetType = 'TargetUnit';
+	abil.CastEffectType = 'Physical';
 	abil.CanCastOnHostiles = true;
 	abil.ShouldPort = true;
 	abil.PhysicalDPS = { 75, 150, 225 }; -- total damage / 5 seconds
@@ -64,15 +96,29 @@ return hero;
 -- Don't copy the comments for the abilities, they should only exist in the AbilityInfo class and in this template.
 -- You can always look at existing hero info files for examples.
 -- The sum threat of a hero + abilities should generally always be 6. There are a few exceptions to this rule, such as Armadon (5 - increases per stack of spine burst), Behemoth (7 - only with ult) and Tempest (10 - only with ult).
---[[
-Available properties:
-]]
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- Required properties: these should be set in every ability instance.
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+-- The threat of the ability. Only one value allowed. Only applied when the ability is off cooldown. For passive abilities add threat to the hero. Keep in mind that any abilities that increase the visual DPS of the hero will automatically increase threat too.
+abil.Threat = 0;
+-- The targeting type of the ability. Only one value allowed. May be used to automatically determine how an ability should be cast.
 -- Passive (also for "Toggle Aura" abilities like SR's Withering Presence), Self (also for "Self Position" abilities like Keeper's Root), AutoCast, TargetUnit, TargetPosition, TargetVector, VectorEntity
-abil.TargetType = 'UNKNOWN';
+abil.TargetType = '';
+
+-- The below value isn't really required: it should be filled if the casteffecttype in the ability.entity file for the abiltiy has been filled. If not then you can skip setting this property. (it's not listed as an optional property since it doesn't share those traits)
+-- You can also provide a table with multiple values, e.g. Tundra's Piercing Shards which is both superior magic and superior physical would be { 'SuperiorMagic', 'SuperiorPhysical' }.
+abil.CastEffectType = ''; -- Magic, Physical, SuperiorMagic, SuperiorPhysical 
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- Optional properties
+-- Most of these properties may also be tables containing different values per level, e.g. abil.CanStun = { false, false, false, true }
+-----------------------------------------------------------------------------------------------------------------------------------------
 
 -- If the TargetType is VectorEntity this should specify what kind of target is optimal (e.g. for Grinex this would be a table: { 'Hero', 'Cliff', 'Tree', 'Building' }, for Rally this is 'Hero').
 -- Possible values: Hero, Cliff, Tree, Building or a combination of them. This does NOT hold different values per levels if a table is provided, but instead holds all possible targets.
-class.VectorEntityTarget = nil;
+abil.VectorEntityTarget = nil;
 
 -- Whether the ability can only be cast on self. Things like Scout's Vanish or Accursed's ult count as such.
 abil.CanCastOnSelf = true;
